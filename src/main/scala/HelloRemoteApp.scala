23deb7a989dece717b497a16cfe7cc5e6094f250
ayub.malik <ayub.malik@gmail.com>
2016-02-04 21:04:19 +0000

    Interim

--- src/main/scala/HelloRemoteApp.scala
+++ src/main/scala/HelloRemoteApp.scala
@@ -7,11 +7,12 @@ import akka.actor.Props
 
 object HelloRemoteApp extends App {
 
-  import scala.concurrent.ExecutionContext.Implicits.global
+  val hostname = if (!args.isEmpty) args(0) else "127.0.0.1"
+
   val root = ConfigFactory.load()
 
   val config = ConfigFactory
-    .parseString(s"akka.remote.netty.tcp.port=2522")
+    .parseString(s"akka.remote.netty.tcp.hostname=$hostname")
     .withFallback(root.getConfig("helloRemote"))
 
   val system = ActorSystem("HelloRemoteSystem", config)
